85f281a07985ed2919ede838b2c34a59cf861ddc
MegaUser <MegaUser@DESKTOP-9QE6CM7>
2017-07-26 16:55:42 +0300

    Added PhantomJS driver

--- TruliaParser/Program.cs
+++ TruliaParser/Program.cs
@@ -15,6 +15,9 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 using TruliaParser;
 using NLog;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium;
+using System.Drawing;
 
 namespace FTParser
 {
@@ -24,7 +27,55 @@ namespace FTParser
 
         static void Main(string[] args)
         {
-            logger.Info("Парсер начал работу.");
+            //logger.Info("Парсер начал работу.");
+            
+            List<string> links = new List<string>();
+            links.Add("https://facebook.com");
+            links.Add("https://vk.com");
+            links.Add("https://mail.ru");
+            links.Add("https://yandex.ru");
+            links.Add("http://4pda.ru");
+           // var driver = new PhantomJSDriver();
+           // driver.Navigate().GoToUrl("https://vk.com");
+           // File.WriteAllText(links[0].Split('/')[2] + ".html", driver.PageSource.ToString(), Encoding.Unicode);
+           // driver.Quit();
+            ParallelOptions options = new ParallelOptions();
+            options.MaxDegreeOfParallelism = 5;
+            Parallel.ForEach(links, options, (link) =>
+              {
+                  var driver = new PhantomJSDriver();
+                  driver.Manage().Window.Size = new Size(1920, 1080);
+                  driver.Navigate().GoToUrl(link);
+                  
+                  driver.GetScreenshot().SaveAsFile(link.Split('/')[2]+".png",ScreenshotImageFormat.Png);
+                  File.WriteAllText(link.Split('/')[2] + ".html", driver.PageSource.ToString(), Encoding.Unicode);
+                  driver.Quit();
+              });
+            //var driver = new PhantomJSDriver();
+            //driver.Navigate().GoToUrl("https://vk.com");
+            //File.WriteAllText("file.html",driver.PageSource.ToString(),Encoding.Unicode);
+            //driver.Quit();
+            Console.WriteLine("Я все скачал!");
+            Console.ReadKey();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /*
             ParallelOptions options = new ParallelOptions();
             options.MaxDegreeOfParallelism = Convert.ToInt32(Resources.MaxDegreeOfParallelism);
             Console.WriteLine("Получаю список неспарсенных регионов...");
@@ -35,8 +86,8 @@ namespace FTParser
             {
                 Console.WriteLine(reg);
             }
-
-
+            */
+            /*
             Parallel.ForEach(regions, options, (reg) =>
             {
                 
@@ -54,7 +105,8 @@ namespace FTParser
 
 
             Console.WriteLine("Работа парсера завершена. Для продолжения нажмите любую клавишу...");
-            Console.ReadKey();
+    */        
+    //Console.ReadKey();
         }
     }
 }
